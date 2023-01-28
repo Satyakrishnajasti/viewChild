9b83f392e197ce7151ed5b71b635ac87104e3f2c
Satytakrishnajasti <satyajasti9@gmail.com>
2023-01-28 23:15:07 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'view-child';
+
+  @ViewChild('content') content?: any;
+  @ViewChild('button') button?: any;
+
+
+  changeColor() {
+    this.content.nativeElement.innerText = 'Welcome';
+  }
+
+  ngAfterViewInit() {
+    this.content.nativeElement.style.color = 'red';
+    this.content.nativeElement.addEventListener('click', this.changeColor());
+  }
+
+
 }
